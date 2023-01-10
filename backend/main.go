97f861109f2763e656f87fde7103fcd7da672ed5
SirivipaSuramanee <siriv@DESKTOP-TJQ5SL6>
2023-01-11 00:15:59 +0700

    add gitignore

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/team04/controller"
+	"github.com/team04/entity"
 )
 
 func main() {
+	entity.SetupDatabase()
 	r := gin.Default()
 	r.GET("/health", controller.Health)
 	r.Run()
