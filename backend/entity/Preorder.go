f14fd398ed2e7ae0996901eef7e0e7652b0214d6
Maprang6224 <Maprang6224@gmail.com>
2023-01-23 20:28:09 +0700

    add entity preorder - close #21

---
+++ backend/entity/Preorder.go
@@ -0,0 +1,32 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// ระบบสั่งซื้อสินค้า pre order
+
+type Preorder struct {
+	gorm.Model
+
+	OwnerID *uint
+	Owner   User `gorm:"references:id;"`
+
+	Name       string
+	Price      int
+	Author     string
+	Edition    int
+	Year       string
+	Quantity   int
+	Totalprice int
+
+	PaymentID *uint
+	Payment   Payment `gorm:"references:id;"`
+
+	Datetime time.Time
+
+	LibrarianID *uint
+	Librarian   Librarian `gorm:"references:id;"`
+}
