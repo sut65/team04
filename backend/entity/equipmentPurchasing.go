2db80c6e204ed97b7d6e55f875033bdce3e52de5
SirivipaSuramanee <siriv@DESKTOP-TJQ5SL6>
2023-01-23 17:55:49 +0700

    add entity EquipmentPurchasing - close #85

---
+++ backend/entity/equipmentPurchasing.go
@@ -0,0 +1,37 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type EquipmentCategory struct {
+	gorm.Model
+	Name string
+
+	EquipmentPurchasing []EquipmentPurchasing `gorm:"foreignKey:EquipmentCategoryID"`
+}
+
+type Company struct {
+	gorm.Model
+	Name string `gorm:"uniqueIndex"`
+
+	EquipmentPurchasing []EquipmentPurchasing `gorm:"foreignKey:CompanyID"`
+}
+
+type EquipmentPurchasing struct {
+	gorm.Model
+	EquipmentName string
+	Amount        uint
+	Date          time.Time
+
+	LibrarianID *uint
+	Librarian   Librarian `gorm:"references:id;"`
+
+	EquipmentCategoryID *uint
+	EquipmentCategory   EquipmentCategory `gorm:"references:id;"`
+
+	CompanyID *uint
+	Company   Company `gorm:"references:id;"`
+}
