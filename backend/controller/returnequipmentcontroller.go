65f8e9f741988ec95bac1b50b7b1c28436490354
chanapornuppamana <ACER@LAPTOP-D882QL77>
2023-01-25 17:42:10 +0700

    update controller of returnequipment

--- backend/controller/returnequipmentcontroller.go
+++ backend/controller/returnequipmentcontroller.go
@@ -1,48 +1,47 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/team04/entity"
 )
 
-// POST /return_equipments
-func CreateReturnEquipment(c *gin.Context) {
-	var returnequipment entity.ReturnEquipment
+// return_equipments
+func CreateReturnEquipment(c *gin.Context) { // c รับข้อมูลมาจาก api
+
+	var returnequipment entity.ReturnEquipment //การประกาศตัวแปรให้เป็นไทป์ที่เราสร้างขึ้นเอง
 	var equipmentstatus entity.EquipmentStatus
 	var borrowequipment entity.BorrowEquipment
 	var librarian entity.Librarian
 
-	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 9 จะถูก bind เข้าตัวแปร returnequipment
+	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 10 จะถูก bind เข้าตัวแปร returnequipment
 	if err := c.ShouldBindJSON(&returnequipment); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
-	}
+	} //การบาย
 
-	// : ค้นหา equipmentstatus ด้วย id
-	if tx := entity.DB().Where("id = ?",
-		returnequipment.EquipmentStatusID).First(&equipmentstatus); tx.RowsAffected == 0 {
+	// 11: ค้นหา equipmentstatus ด้วย id
+	if tx := entity.DB().Where("id = ?", returnequipment.EquipmentStatusID).First(&equipmentstatus); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "equipment status not found"})
 		return
 	}
 
-	// : ค้นหา borrow equipment ด้วย id
-	if tx := entity.DB().Where("id = ?",
-		returnequipment.BorrowEquipmentID).First(&borrowequipment); tx.RowsAffected == 0 {
+	// 13: ค้นหา borrowequipment ด้วย id
+	if tx := entity.DB().Where("id = ?", returnequipment.BorrowEquipmentID).First(&borrowequipment); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "borrow equipment not found"})
 		return
 	}
 
-	// : ค้นหา librarian ด้วย id
-	if tx := entity.DB().Where("id = ?",
-		returnequipment.LibrarianID).First(&librarian); tx.RowsAffected == 0 {
+	// 15: ค้นหา librarian ด้วย id
+	if tx := entity.DB().Where("id = ?", returnequipment.LibrarianID).First(&librarian); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "librarian not found"})
 		return
 	}
 
-	// : สร้าง return equipment
-	ps := entity.ReturnEquipment{
+	// 17: สร้าง borrowequipment
+	BP := entity.ReturnEquipment{
 		EquipmentStatus: equipmentstatus,               // โยงความสัมพันธ์กับ Entity EquipmentStatus
 		Librarian:       librarian,                     // โยงความสัมพันธ์กับ Entity Librarian
 		BorrowEquipment: borrowequipment,               // โยงความสัมพันธ์กับ Entity BorrowEquipment
@@ -51,59 +50,69 @@ func CreateReturnEquipment(c *gin.Context) {
 
 	}
 
-	// : บันทึก
-	if err := entity.DB().Create(&ps).Error; err != nil {
+	// 18: บันทึก
+	if err := entity.DB().Create(&BP).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": ps})
+	c.JSON(http.StatusOK, gin.H{"data": BP}) //ส่ง BP กลับไปตรงที่ fetch ที่เราเรียกใช้
 }
 
-// GET /return_equipments/:id
-func GetReturnEquipment(c *gin.Context) {
-	var returnequipment entity.ReturnEquipment
-	id := c.Param("id")
-	if err := entity.DB().Preload("BorrowEquipment.User").Preload("BorrowEquipment").Preload("EquipmentStatus").Preload("Librarian").Raw("SELECT * FROM return_equipments WHERE id = ?", id).Find(&returnequipment).Error; err != nil {
+// GET return_equipments
+func GetAllReturnEquipment(c *gin.Context) {
+
+	var returnequipment []entity.ReturnEquipment
+
+	if err := entity.DB().Model(&entity.ReturnEquipment{}).Preload("Librarian").Find(&returnequipment).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	c.JSON(http.StatusOK, gin.H{"data": returnequipment})
+
 }
 
-// GET /return_equipments
-func ListReturnEquipments(c *gin.Context) {
-	var returnequipments []entity.ReturnEquipment
-	if err := entity.DB().Preload("BorrowEquipment.User").Preload("BorrowEquipment").Preload("EquipmentStatus").Preload("Librarian").Raw("SELECT * FROM return_equipments").Find(&returnequipments).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": returnequipments})
-}
+// GET return_equipments By ID
+func GetReturnEquipmentByID(c *gin.Context) {
 
-// DELETE /return_equipments/:id
-func DeleteReturnEquipment(c *gin.Context) {
-	id := c.Param("id")
-	if tx := entity.DB().Exec("DELETE FROM return_equipments WHERE id = ?", id); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "returnequipment not found"})
+	var returnequipment entity.ReturnEquipment
+	Id := c.Param("id") //id ที่เราตั้งไว้ใน main.go ที่อยู่หลัง : ตัวอย่าง >> /returnequipment/:id
+	if err := entity.DB().Model(&entity.ReturnEquipment{}).Where("ID = ?", Id).Preload("Librarian").Find(&returnequipment); err.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("ReturnEquipmentID :  Id%s not found.", Id)})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": id})
+
+	c.JSON(http.StatusOK, gin.H{"data": returnequipment})
+
 }
 
 // PATCH /return_equipments
 func UpdateReturnEquipment(c *gin.Context) {
 	var returnequipment entity.ReturnEquipment
+
 	if err := c.ShouldBindJSON(&returnequipment); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	if tx := entity.DB().Where("id = ?", returnequipment.ID).First(&returnequipment); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "return equipment not found"})
+	if tx := entity.DB().Where("id = ?", returnequipment.ID).First(&entity.ReturnEquipment{}); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "return equipment not found"}) //เช็คว่ามีไอดีอยู่ในดาต้าเบสมั้ย
 		return
 	}
 	if err := entity.DB().Save(&returnequipment).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	c.JSON(http.StatusOK, gin.H{"data": returnequipment})
+}
+
+// DELETE return_equipments By id
+func DeleteReturnEquipment(c *gin.Context) {
+	Id := c.Param("id")
+	if tx := entity.DB().Delete(&entity.ReturnEquipment{}, Id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "return equipment ID not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, fmt.Sprintf("ReturnEquipmentID :  %s deleted.", Id))
 }
