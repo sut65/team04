e02f8c6edc1336a7bd85063f68fcc0e1bb21669f
Thanphirom <B6223045@g.sut.ac.th>
2023-01-25 18:01:41 +0700

    edit returnbookcontroller.go - close #159

--- backend/controller/returnbookcontroller.go
+++ backend/controller/returnbookcontroller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,8 +9,8 @@ import (
 )
 
 // POST /return_books
-func CreateReturnBook(c *gin.Context) {
-	var returnbook entity.ReturnBook
+func CreateReturnBook(c *gin.Context) { // c รับข้อมูลมาจาก api
+	var returnbook entity.ReturnBook //การประกาศตัวแปรให้เป็นไทป์ที่เราสร้างขึ้นเอง
 	var lostbook entity.LostBook
 	var borrowbook entity.BorrowBook
 	var librarian entity.Librarian
@@ -23,14 +24,14 @@ func CreateReturnBook(c *gin.Context) {
 	// : ค้นหา Lostbook ด้วย id
 	if tx := entity.DB().Where("id = ?",
 		returnbook.LostBookID).First(&lostbook); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Lostbook not found"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "lostbook not found"})
 		return
 	}
 
 	// : ค้นหา Borrowbook ด้วย id
 	if tx := entity.DB().Where("id = ?",
 		returnbook.BorrowBookID).First(&borrowbook); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Borrowbook not found"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "borrowbook not found"})
 		return
 	}
 
@@ -56,38 +57,28 @@ func CreateReturnBook(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": ps})
+	c.JSON(http.StatusOK, gin.H{"data": ps}) //ส่ง ps กลับไปตรงที่ fetch ที่เราเรียกใช้
 }
 
-// GET /return_books/:id
-func GetReturnBook(c *gin.Context) {
-	var returnbook entity.ReturnBook
-	id := c.Param("id")
-	if err := entity.DB().Preload("BorrowBook.User").Preload("BorrowBook").Preload("LostBook").Preload("Librarian").Raw("SELECT * FROM return_books WHERE id = ?", id).Find(&returnbook).Error; err != nil {
+// GET /return_books
+func GetAllReturnBook(c *gin.Context) {
+	var returnbook []entity.ReturnBook
+	if err := entity.DB().Model(&entity.ReturnBook{}).Preload("BorrowBook.User").Preload("BorrowBook").Preload("LostBook").Preload("Librarian").Find(&returnbook).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": returnbook})
 }
 
-// GET /return_books
-func ListReturnBooks(c *gin.Context) {
-	var Returnbooks []entity.ReturnBook
-	if err := entity.DB().Preload("BorrowBook.User").Preload("BorrowBook").Preload("LostBook").Preload("Librarian").Raw("SELECT * FROM return_books").Find(&Returnbooks).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// GET /return_books/:id
+func GetReturnBookByID(c *gin.Context) {
+	var returnbook entity.ReturnBook
+	Id := c.Param("id") //id ที่เราตั้งไว้ใน main.go ที่อยู่หลัง : ตัวอย่าง >> /return_books/:id
+	if err := entity.DB().Model(&entity.ReturnBook{}).Where("ID = ?", Id).Preload("BorrowBook.User").Preload("BorrowBook").Preload("LostBook").Preload("Librarian").Find(&returnbook); err.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("BookPurchasingID :  Id%s not found.", Id)})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": Returnbooks})
-}
-
-// DELETE /return_books/:id
-func DeleteReturnBook(c *gin.Context) {
-	id := c.Param("id")
-	if tx := entity.DB().Exec("DELETE FROM return_books WHERE id = ?", id); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "returnbook not found"})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": id})
+	c.JSON(http.StatusOK, gin.H{"data": returnbook})
 }
 
 // PATCH /return_book
@@ -97,8 +88,8 @@ func UpdateReturnBook(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	if tx := entity.DB().Where("id = ?", returnbook.ID).First(&returnbook); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Returnbook not found"})
+	if tx := entity.DB().Where("id = ?", returnbook.ID).First(&entity.ReturnBook{}); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "returnbook not found"}) //เช็คว่ามีไอดีอยู่ในดาต้าเบสมั้ย
 		return
 	}
 	if err := entity.DB().Save(&returnbook).Error; err != nil {
@@ -106,4 +97,14 @@ func UpdateReturnBook(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": returnbook})
+}
+
+// DELETE /return_books/:id
+func DeleteReturnBook(c *gin.Context) {
+	Id := c.Param("id")
+	if tx := entity.DB().Delete(&entity.ReturnBook{}, Id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "returnbook ID not found"})
+		return
+	}
+	c.JSON(http.StatusOK, fmt.Sprintf("ReturnBookID :  %s deleted.", Id))
 }
