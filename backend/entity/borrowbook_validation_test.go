3afec6aeea683f54742e2e2555a9e51f763104a7
Thanphirom <B6223045@g.sut.ac.th>
2023-02-06 20:09:40 +0700

    add TESTReturn_DayMustNotBePast in borrowbook_validation_test.go - close #65

--- backend/entity/borrowbook_validation_test.go
+++ backend/entity/borrowbook_validation_test.go
@@ -80,3 +80,28 @@ func TestBorb_DayMustNotBePast(t *testing.T) {
 	// err.Error ต้องมี error message แสดงออกมา
 	g.Expect(err.Error()).To(Equal("วันที่ยืมหนังสือต้องไม่เป็นวันในอดีต"))
 }
+
+// ตรวจสอบวันเวลาวันกำหนดคืนหนังสือต้องไม่เป็นอดีต
+func TestReturn_DayMustNotBePast(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	borrowbook := BorrowBook{
+		Borb_Day:       time.Now(),
+		Return_Day:     time.Now().Add(-24 * time.Hour), //ผิด
+		Color_Bar:      "สีเเดง",
+		Borb_Frequency: 1,
+	}
+
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(borrowbook)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("วันกำหนดคืนหนังสือต้องไม่เป็นวันในอดีต"))
+
+}
