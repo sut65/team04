c85740625f4abfe05b60a75b994d98d4235e13d7
Maprang6224 <Maprang6224@gmail.com>
2023-02-06 18:06:10 +0700

    add NoteTel Must Be Invalid Pattern - close #97

--- backend/entity/confirmation_validation_test.go
+++ backend/entity/confirmation_validation_test.go
@@ -26,3 +26,31 @@ func TestConfirmationNoteNameNotBlank(t *testing.T) {
 		g.Expect(err.Error()).To(Equal("NoteName cannot be blank"))
 	})
 }
+
+// ตรวจสอบหมายเหตุเบอร์ผู้รับ ต้องขึ้นต้นด้วย '0' ตามด้วย '6','8','9' และตามด้วย '0'-'9' จำนวน 8 ตัว
+func TestNoteTelMustBeInValidPattern(t *testing.T) {
+	g := NewGomegaWithT(t)
+	fixtures := []string{
+		"0000000000",  // เป็น 0
+		"0200000000",  // ขึ้นต้นด้วย 0 ตามด้วย 2 และตามด้วย string 8 ตัว
+		"090-0000000", // มีขีดคั่น
+		"080000000",   // ขึ้นต้นด้วย 0 ตามด้วย 8 และตามด้วย string 7 ตัว
+		"9912345678",  // ขึ้นต้นด้วย 9
+		"090",         // ตัวอักษร 3 ตัว
+		"0",           // ตัวอักษร 1 ตัว
+	}
+
+	for _, fixture := range fixtures {
+		c := Confirmation{
+			NoteName: "Maprang Saengarun",
+			NoteTel:  fixture, //ผิด
+			Datetime: time.Now(),
+		}
+
+		// ตรวจสอบด้วย govalidator
+		ok, err := govalidator.ValidateStruct(c)
+		g.Expect(ok).ToNot(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("NoteTel invalid"))
+	}
+}
