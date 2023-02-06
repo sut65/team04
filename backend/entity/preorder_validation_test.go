07923fbc5716d5e7ae090b014b325bee740f63ac
Maprang6224 <Maprang6224@gmail.com>
2023-02-06 14:18:28 +0700

    update Preorder Price Not Zero - close #53

--- backend/entity/preorder_validation_test.go
+++ backend/entity/preorder_validation_test.go
@@ -33,3 +33,36 @@ func TestPreorderNameNotBlank(t *testing.T) {
 	// err.Error ต้องมี error message แสดงออกมา
 	g.Expect(err.Error()).To(Equal("Name cannot be blank"))
 }
+
+// ตรวจสอบราคาหนังสือ ต้องเป็นตัวเลขมากกว่า 0 - ถ้าไม่ตรงจะ error
+func TestPreorderPriceNotZero(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	fixture := []int{
+		-1, 0, 10000}
+
+	for _, price := range fixture {
+		pr := Preorder{
+			Name:       "Css",
+			Price:      price, //ผิด
+			Author:     "maprang",
+			Edition:    1,
+			Year:       "2010",
+			Quantity:   1,
+			Totalprice: 150,
+		}
+
+		//ตรวจสอบด้วย govalidator
+		ok, err := govalidator.ValidateStruct(pr)
+
+		//ok ต้องไม่เป็นค่า true แปลว่าต้องจับ err ได้
+		g.Expect(ok).ToNot(BeTrue())
+
+		// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+		g.Expect(err).ToNot(BeNil())
+
+		// err.Error ต้องมี error message แสดงออกมา
+		g.Expect(err.Error()).To(Equal("Price must greater than zero"))
+
+	}
+}
