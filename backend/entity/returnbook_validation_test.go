6c8ec8ded581cd93d5a6c66fb1ea527b81ec5999
Thanphirom <B6223045@g.sut.ac.th>
2023-02-07 17:25:11 +0700

    add validation TestBook_ConditionNotBlank in returnbook_validation_test.go - close #78

--- backend/entity/returnbook_validation_test.go
+++ backend/entity/returnbook_validation_test.go
@@ -28,3 +28,26 @@ func TestReturnBookCorrect(t *testing.T) {
 	g.Expect(err).To((BeNil()))
 
 }
+
+// ตรวจสอบค่าว่างของสภาพหนังสือแล้วต้องเจอ Error
+func TestBook_ConditionNotBlank(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	returnbook := ReturnBook{
+		Current_Day:    time.Now(),
+		Late_Number:    0,
+		Book_Condition: "", //ผิด
+	}
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(returnbook)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("กรุณากรอกข้อมูลสภาพหนังสือ"))
+
+}
