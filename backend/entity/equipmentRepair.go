4601409ba79c98c53c76c98fd46f950876d5a523
thanaponkhanoon <thanaponkhanoon@gmail.com>
2023-02-08 17:38:58 +0700

    validationtest equipment not not blank - close #102

--- backend/entity/equipmentRepair.go
+++ backend/entity/equipmentRepair.go
@@ -4,20 +4,44 @@ import (
 	"time"
 
 	"gorm.io/gorm"
+	"github.com/asaskevich/govalidator"
 )
 
 type EquipmentRepair struct {
 	gorm.Model
 	EquipmentPurchasingID *uint
-	EquipmentPurchasing   EquipmentPurchasing `gorm:"references:id;"`
+	EquipmentPurchasing   EquipmentPurchasing `gorm:"references:id" valid:"-"`
 
 	LevelID *uint
-	Level   Level	`gorm:"references:id;"`
+	Level   Level	`gorm:"references:id" valid:"-"`
 
-	Date time.Time
+	Date time.Time	`valid:"Past~วันที่แจ้งซ่อมอุปกรณ์ต้องไม่เป็นวันในอดีต,Future~วันที่แจ้งซ่อมอุปกรณ์ต้องไม่เป็นวันในอนาคต"`
 
-	Note	string
+	Note	string	`valid:"required~Note cannot be blank"`
 
 	LibrarianID *uint
-	Librarian   Librarian	`gorm:"references:id;"`
+	Librarian   Librarian	`gorm:"references:id" valid:"-"`
+}
+
+//ฟังก์ชันที่จะใช่ในการ validation EntryTime
+func init() {
+    govalidator.CustomTypeTagMap.Set("Past", func(i interface{}, context interface{}) bool {
+        t := i.(time.Time)
+		return t.After(time.Now().Add(time.Minute*-24)) || t.Equal(time.Now())
+        //return t.Before(time.Now())
+    })
+
+    // govalidator.CustomTypeTagMap.Set("Now", func(i interface{}, context interface{}) bool {
+    //     t := i.(time.Time)
+    //     now := time.Now()
+    //     return now.Before(time.Time(t))
+    // })
+
+	govalidator.CustomTypeTagMap.Set("Future", func(i interface{}, context interface{}) bool {
+		t := i.(time.Time)
+		return t.Before(time.Now().Add(time.Minute*24)) || t.Equal(time.Now())
+
+		// now := time.Now()
+		// return now.Before(time.Time(t))
+	})
 }
