4af886fda101c4b59e1636be04e4c5fecf576b4a
SirivipaSuramanee <siriv@DESKTOP-TJQ5SL6>
2023-02-09 15:21:15 +0700

    validation ตรวจสอบค่าว่างของ BookName - close #35

--- backend/entity/bookPurchasing_validation_test.go
+++ backend/entity/bookPurchasing_validation_test.go
@@ -29,3 +29,27 @@ func TestBookPurchasingCorrect(t *testing.T) {
 	g.Expect(err).To((BeNil()))
 
 }
+
+// ตรวจสอบค่าว่างของ BookName แล้วต้องเจอ Error
+func TestBookNameNotBlank(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	bookname := BookPurchasing{
+		BookName:   "",
+		AuthorName: "อาจารย์กร",
+		Amount:     70,
+		Date:       time.Now(),
+	}
+
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(bookname)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("กรุณากรอกชื่อหนังสือ"))
+}
