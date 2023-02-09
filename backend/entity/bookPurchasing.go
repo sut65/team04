598a5ad1c82bf802a9131956d6a25b12d8e96bcd
SirivipaSuramanee <siriv@DESKTOP-TJQ5SL6>
2023-02-09 14:24:13 +0700

    edit bookPurchasing.go

--- backend/entity/bookPurchasing.go
+++ backend/entity/bookPurchasing.go
@@ -3,6 +3,7 @@ package entity
 import (
 	"time"
 
+	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
 )
 
@@ -19,8 +20,8 @@ type Librarian struct {
 	Forfeit          []Forfeit         `gorm:"foreignKey:PaymentID"`
 	BorrowEquipments []BorrowEquipment `gorm:"foreignKey:LibrarianID"`
 	ReturnEquipments []ReturnEquipment `gorm:"foreignKey:LibrarianID"`
-	BookRepair		 []BookRepair      `gorm:"foreignKey:LibrarianID"`
-	EquipmentRepair	 []EquipmentRepair      `gorm:"foreignKey:LibrarianID"`
+	BookRepair       []BookRepair      `gorm:"foreignKey:LibrarianID"`
+	EquipmentRepair  []EquipmentRepair `gorm:"foreignKey:LibrarianID"`
 
 	Preorder     []Preorder     `gorm:"foreignKey:LibrarianID"`
 	Confirmation []Confirmation `gorm:"foreignKey:LibrarianID"`
@@ -42,20 +43,38 @@ type Publisher struct {
 
 type BookPurchasing struct {
 	gorm.Model
-	BookName   string
-	AuthorName string
-	Amount     uint
-	Date       time.Time
+	BookName   string    `valid:"required~กรุณากรอกชื่อหนังสือ"`
+	AuthorName string    `valid:"required~กรุณากรอกชื่อผู้แต่ง"`
+	Amount     uint      `valid:"required~จำนวนหนังสือต้องมากกว่า 0 กรุณาลองใหม่อีกครั้ง,MoreThanZero~จำนวนหนังสือต้องมากกว่า 0 กรุณาลองใหม่อีกครั้ง"`
+	Date       time.Time `valid:"present~วันที่จัดซื้อหนังสือต้องเป็นปัจจุบัน กรุณาลองใหม่อีกครั้ง"`
 
 	LibrarianID *uint
-	Librarian   Librarian `gorm:"references:id;"`
+	Librarian   Librarian `gorm:"references:id" valid:"-"`
 
 	BookCategoryID *uint
-	BookCategory   BookCategory `gorm:"references:id;"`
+	BookCategory   BookCategory `gorm:"references:id" valid:"-"`
 
 	PublisherID *uint
-	Publisher   Publisher `gorm:"references:id;"`
+	Publisher   Publisher `gorm:"references:id" valid:"-"`
 
 	BorrowBooks []BorrowBook `gorm:"foreignKey:BookPurchasingID"`
-	BookRepair []BookRepair `gorm:"foreignKey:BookPurchasingID"`
+	BookRepair  []BookRepair `gorm:"foreignKey:BookPurchasingID"`
+}
+
+// ฟังก์ชันที่จะใช่ในการ validation EntryTime
+func init() {
+	govalidator.CustomTypeTagMap.Set("present", func(i interface{}, context interface{}) bool {
+		t := i.(time.Time)
+		return t.After(time.Now().Add(2-time.Minute)) && t.Before(time.Now().Add(2+time.Minute))
+	})
+
+	govalidator.CustomTypeTagMap.Set("MoreThanZero", func(i interface{}, context interface{}) bool {
+		t := i.(uint)
+		if t <= 0 {
+			return false
+		} else {
+			return true
+		}
+	})
+
 }
