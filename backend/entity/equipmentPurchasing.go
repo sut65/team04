dd34475f34df73287d6c2e68578306b2961ec9f2
SirivipaSuramanee <siriv@DESKTOP-TJQ5SL6>
2023-02-10 12:54:39 +0700

    add function edit  button

--- backend/entity/equipmentPurchasing.go
+++ backend/entity/equipmentPurchasing.go
@@ -3,6 +3,7 @@ package entity
 import (
 	"time"
 
+	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
 )
 
@@ -22,9 +23,9 @@ type Company struct {
 
 type EquipmentPurchasing struct {
 	gorm.Model
-	EquipmentName string
-	Amount        uint
-	Date          time.Time
+	EquipmentName string    `valid:"required~กรุณากรอกชื่ออุปกรณ์"`
+	Amount        uint      `valid:"required~จำนวนอุปกรณ์ต้องมากกว่า 0 กรุณาลองใหม่อีกครั้ง,MoreThanZero~จำนวนอุปกรณ์ต้องมากกว่า 0 กรุณาลองใหม่อีกครั้ง"`
+	Date          time.Time `valid:"present~วันที่จัดซื้ออุปกรณ์ต้องเป็นปัจจุบัน กรุณาลองใหม่อีกครั้ง"`
 
 	LibrarianID *uint
 	Librarian   Librarian `gorm:"references:id;"`
@@ -36,5 +37,23 @@ type EquipmentPurchasing struct {
 	Company   Company `gorm:"references:id;"`
 
 	BorrowEquipments []BorrowEquipment `gorm:"foreignKey:EquipmentPurchasingID"`
-	EquipmentRepair []EquipmentRepair `gorm:"foreignKey:EquipmentPurchasingID"`
+	EquipmentRepair  []EquipmentRepair `gorm:"foreignKey:EquipmentPurchasingID"`
+}
+
+// ฟังก์ชันที่จะใช่ในการ validation EntryTime
+func init() {
+	govalidator.CustomTypeTagMap.Set("present", func(i interface{}, context interface{}) bool {
+		t := i.(time.Time)
+		return t.After(time.Now().Add(10-time.Minute)) && t.Before(time.Now().Add(10+time.Minute))
+	})
+
+	govalidator.CustomTypeTagMap.Set("MoreThanZero", func(i interface{}, context interface{}) bool {
+		t := i.(uint)
+		if t <= 0 {
+			return false
+		} else {
+			return true
+		}
+	})
+
 }
