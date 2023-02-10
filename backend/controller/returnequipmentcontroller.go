d42c1711b5c4c847c979323048518e9423fa5926
chanapornuppamana <ACER@LAPTOP-D882QL77>
2023-02-11 05:51:41 +0700

    update returnequipmentcontroller.go - close #280

--- backend/controller/returnequipmentcontroller.go
+++ backend/controller/returnequipmentcontroller.go
@@ -17,11 +17,23 @@ func CreateReturnEquipment(c *gin.Context) { // c รับข้อมูลม
 	var borrowequipment entity.BorrowEquipment
 	var librarian entity.Librarian
 
-	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 10 จะถูก bind เข้าตัวแปร returnequipment
+	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 8 จะถูก bind เข้าตัวแปร returnequipment
 	if err := c.ShouldBindJSON(&returnequipment); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	} //การบาย
+
+	// 9: ค้นหา borrowequipment ด้วย id
+	if tx := entity.DB().Where("id = ?", returnequipment.BorrowEquipmentID).First(&borrowequipment); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "borrow equipment not found"})
+		return
+	}
+
+	// 10: อัพเดทคอลัมน์ TrackingCheck ว่าการคืนอุปกรณ์ถูกประเมินแล้ว
+	if tx := entity.DB().Model(&borrowequipment).Update("TrackingCheck", true); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "borrowequipment not found"})
+		return
+	}
 
 	// 11: ค้นหา equipmentstatus ด้วย id
 	if tx := entity.DB().Where("id = ?", returnequipment.EquipmentStatusID).First(&equipmentstatus); tx.RowsAffected == 0 {
@@ -29,40 +41,30 @@ func CreateReturnEquipment(c *gin.Context) { // c รับข้อมูลม
 		return
 	}
 
-	// 13: ค้นหา borrowequipment ด้วย id
-	if tx := entity.DB().Where("id = ?", returnequipment.BorrowEquipmentID).First(&borrowequipment); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "borrow equipment not found"})
-		return
-	}
-
-	// 11: อัพเดทคอลัมน์ TrackingCheck ว่าการคืนอุปกรณ์ถูกประเมินแล้ว
-	if tx := entity.DB().Model(&borrowequipment).Update("TrackingCheck", true); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "borrowequipment not found"})
-		return
-	}
-
-	// 15: ค้นหา librarian ด้วย id
+	// 12: ค้นหา librarian ด้วย id
 	if tx := entity.DB().Where("id = ?", returnequipment.LibrarianID).First(&librarian); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "librarian not found"})
 		return
 	}
 
-	// 17: สร้าง borrowequipment
+	localtime := returnequipment.Return_Day.Local()
+	// 13: สร้าง borrowequipment
 	BP := entity.ReturnEquipment{
 		EquipmentStatus: equipmentstatus,               // โยงความสัมพันธ์กับ Entity EquipmentStatus
 		Librarian:       librarian,                     // โยงความสัมพันธ์กับ Entity Librarian
 		BorrowEquipment: borrowequipment,               // โยงความสัมพันธ์กับ Entity BorrowEquipment
-		Return_Day:      returnequipment.Return_Day,    //ตั้งค่าฟิลด์ Return_Day
+		Return_Day:      localtime,                     //ตั้งค่าฟิลด์ Return_Day
 		Return_Detail:   returnequipment.Return_Detail, //ตั้งค่าฟิลด์ Return_Detail
 
 	}
 
+	//func สำหรับเช็ค validate
 	if _, err := govalidator.ValidateStruct(returnequipment); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	// 18: บันทึก
+	// 14: บันทึก
 	if err := entity.DB().Create(&BP).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -106,10 +108,18 @@ func UpdateReturnEquipment(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	if tx := entity.DB().Where("id = ?", returnequipment.ID).First(&entity.ReturnEquipment{}); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "return equipment not found"}) //เช็คว่ามีไอดีอยู่ในดาต้าเบสมั้ย
 		return
 	}
+
+	//Validate
+	if _, err := govalidator.ValidateStruct(returnequipment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
 	if err := entity.DB().Save(&returnequipment).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
