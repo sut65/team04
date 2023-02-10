9ec686c9a1a959255664e2e1f4ddc10cfaca9110
SirivipaSuramanee <siriv@DESKTOP-TJQ5SL6>
2023-02-10 12:48:55 +0700

    validation ตรวจสอบค่าว่าง

--- backend/entity/equipmentPurchasing_validation_test.go
+++ backend/entity/equipmentPurchasing_validation_test.go
@@ -28,3 +28,26 @@ func TestEquipmentPurchasingCorrect(t *testing.T) {
 	g.Expect(err).To((BeNil()))
 
 }
+
+// ตรวจสอบค่าว่างของ EquipmentName แล้วต้องเจอ Error
+func TestEquipmentNameNotBlank(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	equipmentname := EquipmentPurchasing{
+		EquipmentName: "",
+		Amount:        50,
+		Date:          time.Now(),
+	}
+
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(equipmentname)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("กรุณากรอกชื่ออุปกรณ์"))
+}
