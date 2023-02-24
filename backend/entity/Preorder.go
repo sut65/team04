0c5a32bf1a2603a1ce96b9e6effa50e129b3a879
Maprang6224 <Maprang6224@gmail.com>
2023-02-24 21:00:53 +0700

    modified unit test preorder

--- backend/entity/Preorder.go
+++ backend/entity/Preorder.go
@@ -18,7 +18,7 @@ type Preorder struct {
 	Price      int    `valid:"required~ราคาหนังสือไม่ถูกต้อง, range(1|9999)~ราคาหนังสือไม่ถูกต้อง,"`
 	Author     string `valid:"required~กรุณากรอกชื่อผู้แต่งหนังสือ"`
 	Edition    int    `valid:"required~จำนวนครั้งที่พิมพ์ไม่ถูกต้อง, range(1|9999)~จำนวนครั้งที่พิมพ์ไม่ถูกต้อง,"`
-	Year       string `valid:"required~ปีที่พิมพ์ไม่ถูกต้อง, matches(^2([5|0])([0-9]{2}$))~ปีที่พิมพ์ไม่ถูกต้อง"`
+	Year       string `valid:"required~ปีที่พิมพ์ไม่ถูกต้อง, matches(^(-)|(2([5|0])([0-9]{2})$))~ปีที่พิมพ์ไม่ถูกต้อง"`
 	Quantity   int    `valid:"required~จำนวนหนังสือควรมีตั้งแต่ 1-5 เท่านั้น, range(1|5)~จำนวนหนังสือควรมีตั้งแต่ 1-5 เท่านั้น"`
 	Totalprice int    `valid:"required~ราคารวมทั้งหมดไม่ถูกต้อง, range(1|9999)~ราคารวมทั้งหมดไม่ถูกต้อง,"`
 
